cf9760141fa1bb0a5b6c5b829d389c3b4f971d08
Euaell <euaelmeko@gmail.com>
2025-01-25 18:24:13 +0300

    chore: login button feedback on loading

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -95,9 +95,30 @@ export default function Page() {
 						<button
 							type="submit"
 							disabled={loading}
-							className="bg-blue-500 text-white rounded-lg py-1 px-6"
+							className="bg-blue-500 text-white rounded-lg py-1 px-6 flex items-center"
 						>
-							Login
+							{loading && (
+								<svg
+									className="w-4 h-4 mr-2 animate-spin"
+									fill="none"
+									viewBox="0 0 24 24"
+								>
+									<circle
+										className="opacity-25"
+										cx="12"
+										cy="12"
+										r="10"
+										stroke="currentColor"
+										strokeWidth="4"
+									></circle>
+									<path
+										className="opacity-75"
+										fill="currentColor"
+										d="M4 12a8 8 0 018-8v8H4z"
+									></path>
+								</svg>
+							)}
+							{loading ? "loading..." : "Login"}
 						</button>
 					</div>
 				</form>
