35f7a2319b7ab2aa8c9859a9c0a1c61dbc4f291f
Euael <euaelmeko@gmail.com>
2025-03-19 12:49:44 +0300

    chore: meal plan fix auth

--- middleware.ts
+++ middleware.ts
@@ -22,9 +22,9 @@ export function middleware(request: NextRequest) {
 	// }
 
 	// If trying to access a protected path without a token, redirect to the login page
-	// if (!isPublicPath && !token) {
-	// 	return NextResponse.redirect(new URL('/login?callbackUrl=' + path + (callbackUrl ? '&callbackUrl=' + callbackUrl : ''), request.nextUrl))
-	// }
+	if (!isPublicPath && !token) {
+		return NextResponse.redirect(new URL('/login?callbackUrl=' + path + (callbackUrl ? '&callbackUrl=' + callbackUrl : ''), request.nextUrl))
+	}
 
 	// TODO: Add logic to check if the token is valid and not expired
 		
