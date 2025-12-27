df382eca62b29b165b007dbdb60972b9d8103743
euaell <euaelmeko@gmail.com>
2025-12-27 19:51:14 +0300

    refactor: remove unused UI component exports from index file

--- frontend/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Button } from './Button';
-export type { ButtonProps } from './Button';
-
-export { default as Card, CardHeader, CardTitle, CardDescription, CardContent } from './Card';
-export type { CardProps } from './Card';
-
-export { default as Input } from './Input';
-export type { InputProps } from './Input';
