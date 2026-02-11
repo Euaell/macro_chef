53aa6a9d51b8813d6dabee08aef773a0f5e2040b
euaell <euaelmeko@gmail.com>
2026-02-11 12:07:08 +0300

    chore: clean up package dependencies, remove unused safety check and Zod schema generation scripts

--- frontend/components/ui/label.tsx
+++ frontend/components/ui/label.tsx
@@ -2,9 +2,9 @@
 
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
+import { cn, cva } from "@/lib/utils"
 
-import { cn } from "@/lib/utils"
+type VariantProps<T> = T extends (props: infer P) => string ? P : never
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
