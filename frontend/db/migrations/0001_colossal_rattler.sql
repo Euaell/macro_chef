fe36204246d1419be34561947e21223766d1ac6d
euaell <euaelmeko@gmail.com>
2026-03-02 15:05:13 +0300

    feat: create rateLimit table and update schema for last_request field

--- frontend/db/migrations/0001_colossal_rattler.sql
+++ frontend/db/migrations/0001_colossal_rattler.sql
@@ -1,8 +1,6 @@
-DROP TABLE IF EXISTS "rateLimit";
---> statement-breakpoint
 CREATE TABLE "rateLimit" (
 	"id" varchar(255) PRIMARY KEY NOT NULL,
 	"key" varchar(255) NOT NULL,
 	"count" integer NOT NULL,
-	"last_request" integer NOT NULL
+	"last_request" bigint NOT NULL
 );
